f2b1a075addd67ad634a39f692b463fdd68d9e3e
alsm6169 <alsm6169@gmail.com>
2024-04-18 23:35:09 +0200

    pep8 compatible + print eod cash

--- lean_base/strategy_bb_rsi/main.py
+++ lean_base/strategy_bb_rsi/main.py
@@ -1,23 +1,21 @@
 # region imports
-import pandas as pd
 from AlgorithmImports import *
 from datetime import datetime
 import json
-import os
 LEAN_RESULTS_DIR ='/Results'
 LEAN_BASE_DIR = '/LeanCLI/'
 # endregion
 
 class SpyBB(QCAlgorithm):
 
-    def Initialize(self):
+    def initialize(self):
 
-        # self.Log(f'getcwd: {os.getcwd()}')
+        # self.log(f'getcwd: {os.getcwd()}')
         # dir_obj = os.scandir('/')
         # sub_dir = [ files.name for files in dir_obj if files.is_dir()]
-        # self.Log(f'Initialize scandir(/) results:\n {sub_dir}')
+        # self.log(f'Initialize scandir(/) results:\n {sub_dir}')
         # for current_path, folders, files in os.walk('/LeanCLI'):
-        #    self.Log(f'current_path, folders, files\n '
+        #    self.log(f'current_path, folders, files\n '
         #             f'{current_path}, {folders}, {files}')
         # Convert JSON string to Pandas DataFrame
         # Retrieve JSON data from the file
@@ -28,9 +26,9 @@ class SpyBB(QCAlgorithm):
         #with open("config.json", "r") as file:
         strategy_config = LEAN_BASE_DIR + "strategy_config/sconfig.json"
         with open(strategy_config, "r") as file:
-            self.Log(f'reading {strategy_config}')
+            self.log(f'reading {strategy_config}')
             json_obj = json.load(file)
-            self.Log(json.dumps(json_obj, indent=4))
+            self.log(json.dumps(json_obj, indent=4))
             start_date_str = json_obj["start_date"] if "start_date" in json_obj else start_date_str
             end_date_str = json_obj["end_date"] if "end_date" in json_obj else end_date_str
             trade_symbol = json_obj["trade_symbol"] if "trade_symbol" in json_obj else trade_symbol
@@ -39,31 +37,37 @@ class SpyBB(QCAlgorithm):
         start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
         end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
 
-        self.Log(f"Environment Info: "
+        self.log(f"Environment Info: "
                  f"start_date={start_date},end_date={end_date},trade_symbol={trade_symbol},"
                  f"benchmark_symbol={self.benchmark_symbol}")
 
-        self.SetStartDate(start_date.year, start_date.month, start_date.day)
-        self.SetEndDate(end_date.year, end_date.month, end_date.day)  # (2022, 1, 1)  # Set End Date
-        self.SetCash(100000)  # Set Strategy Cash
+        self.set_start_date(start_date.year, start_date.month, start_date.day)
+        self.set_end_date(end_date.year, end_date.month, end_date.day)  # (2022, 1, 1)  # Set End Date
+        self.set_cash(100000)  # Set Strategy Cash
 
         length = 30
-        self.symbol = self.AddEquity(trade_symbol, Resolution.Hour).Symbol
-        self.bb = self.BB(self.symbol, length, 2, MovingAverageType.Simple, Resolution.Daily)
-        self.rsi = self.RSI(self.symbol, length, resolution=Resolution.Daily)
+        self.symbol = self.add_equity(trade_symbol, Resolution.HOUR).symbol
+        self.bb_ = self.bb(self.symbol, length, 2, MovingAverageType.SIMPLE, Resolution.DAILY)
+        self.rsi_ = self.rsi(self.symbol, length, resolution=Resolution.DAILY)
         # self.sma = self.SMA(self.spy, length, Resolution.Daily)
         # History warm up for shortcut helper BollingerBand & SMA indicator
-        self.WarmUpIndicator(self.symbol, self.bb)
-        self.WarmUpIndicator(self.symbol, self.rsi)
+        self.warm_up_indicator(self.symbol, self.bb_)
+        self.warm_up_indicator(self.symbol, self.rsi_)
         # long method to warm the indicator
         # closing_prices = self.History(self.spy, length, Resolution.Hour)["close"]
         # for time, price in closing_prices.loc[self.spy].items():
-        #    self.bb.Update(time, price)
+        #    self.bb_.Update(time, price)
         # self.sma.Update(time, price)
 
-        self.SetBenchmark(self.benchmark_symbol)
-        self.initialPortfolioValue = self.Portfolio.TotalPortfolioValue
-        self.initialBenchmarkPrice = None
+        self.set_benchmark(self.benchmark_symbol)
+        self.initial_portfolio_value = self.portfolio.total_portfolio_value
+        self.initial_benchmark_price = None
+
+        # Schedule an event to fire at the end of each trading day
+        self.schedule.on(self.date_rules.every_day(trade_symbol),
+                         self.time_rules.before_market_close(trade_symbol),
+                         self.get_eod_stats)
+
 
         self.df_order_plot = pd.DataFrame(columns=['Time',
                                                    'OrderID', 'Symbol',
@@ -74,24 +78,24 @@ class SpyBB(QCAlgorithm):
                                                    'bb-UpperBand', 'bb-LowerBand',
                                                    'Benchmark'])
 
-        stockPlot = Chart('Trade Plot')
-        stockPlot.AddSeries(Series('Buy', SeriesType.Scatter, '$',
-                                   Color.Green, ScatterMarkerSymbol.Triangle))
-        stockPlot.AddSeries(Series('Sell', SeriesType.Scatter, '$',
-                                   Color.Red, ScatterMarkerSymbol.TriangleDown))
-        stockPlot.AddSeries(Series('Liquidate', SeriesType.Scatter, '$',
-                                   Color.Blue, ScatterMarkerSymbol.Diamond))
-        self.AddChart(stockPlot)
+        stock_plot = Chart('Trade Plot')
+        stock_plot.add_series(Series('Buy', SeriesType.SCATTER, '$',
+                                   Color.green, ScatterMarkerSymbol.TRIANGLE))
+        stock_plot.add_series(Series('Sell', SeriesType.SCATTER, '$',
+                                   Color.red, ScatterMarkerSymbol.TRIANGLE_DOWN))
+        stock_plot.add_series(Series('Liquidate', SeriesType.SCATTER, '$',
+                                   Color.blue, ScatterMarkerSymbol.DIAMOND))
+        self.add_chart(stock_plot)
         returnPlot = Chart('Cumulative Return Comparison')
-        self.AddChart(returnPlot)
+        self.add_chart(returnPlot)
 
-    def OnData(self, slice: Slice):
+    def on_data(self, slice: Slice):
         """OnData event is the primary entry point for your algorithm.
             Each new data point will be pumped in here.
             Arguments:
-                data: Slice object keyed by symbol containing the stock data
+                slice: Slice object keyed by symbol containing the stock data
         """
-        if not self.bb.IsReady:
+        if not self.bb_.IsReady:
             return
 
         # if slice.ContainsKey(self.symbol) and slice[self.symbol] is not None:
@@ -101,75 +105,86 @@ class SpyBB(QCAlgorithm):
         # data = slice[self.spy]
         price = slice[self.symbol].Price
         # sym_obj = slice[self.symbol]
-        # self.Debug(f'Symbol: {sym_obj.Symbol}, Time: {sym_obj.Time}, Price:{sym_obj.Price}, '
+        # self.debug(f'Symbol: {sym_obj.Symbol}, Time: {sym_obj.Time}, Price:{sym_obj.Price}, '
         #           f'Open: {sym_obj.Open}, High: {sym_obj.High}, '
         #           f'Low: {sym_obj.Low}, Close: {sym_obj.Close}')
-        # self.Debug(f'{self.Time}, {price},'
-        #            f'{self.bb.MiddleBand.Current.Value}, {self.bb.UpperBand.Current.Value},'
-        #            f'{self.bb.LowerBand.Current.Value},'
+        # self.debug(f'{self.Time}, {price},'
+        #            f'{self.bb_.MiddleBand.Current.Value}, {self.bb_.UpperBand.Current.Value},'
+        #            f'{self.bb_.LowerBand.Current.Value},'
         #            f'{self.Benchmark.Evaluate(self.Time)}')
 
-        self.Plot("Trade Plot", "Price", price)
-        self.Plot("Trade Plot", "bb-MiddleBand", self.bb.MiddleBand.Current.Value)
-        self.Plot("Trade Plot", "bb-UpperBand", self.bb.UpperBand.Current.Value)
-        self.Plot("Trade Plot", "bb-LowerBand", self.bb.LowerBand.Current.Value)
-        self.Plot("Trade Plot", "Benchmark", self.Benchmark.Evaluate(self.Time))
+        self.plot("Trade Plot", "Price", price)
+        self.plot("Trade Plot", "bb-MiddleBand", self.bb_.MiddleBand.Current.Value)
+        self.plot("Trade Plot", "bb-UpperBand", self.bb_.UpperBand.Current.Value)
+        self.plot("Trade Plot", "bb-LowerBand", self.bb_.LowerBand.Current.Value)
+        self.plot("Trade Plot", "Benchmark", self.benchmark.evaluate(self.time))
         # self.Plot("Trade Plot", "SMA", self.sma.Current.Value)
-        self.df_timeseries.loc[len(self.df_timeseries)] = [self.Time,price,
-                                                           self.bb.MiddleBand.Current.Value,
-                                                           self.bb.UpperBand.Current.Value,
-                                                           self.bb.LowerBand.Current.Value,
-                                                           self.Benchmark.Evaluate(self.Time)]
+        self.df_timeseries.loc[len(self.df_timeseries)] = [self.time,price,
+                                                           self.bb_.MiddleBand.Current.Value,
+                                                           self.bb_.UpperBand.Current.Value,
+                                                           self.bb_.LowerBand.Current.Value,
+                                                           self.benchmark.evaluate(self.time)]
 
-        currentBenchmarkPrice = self.Benchmark.Evaluate(self.Time)
-        if self.initialBenchmarkPrice is None:
-            self.initialBenchmarkPrice = currentBenchmarkPrice
-        benchmarkReturn = (currentBenchmarkPrice / self.initialBenchmarkPrice) - 1
-        self.Plot("Cumulative Return Comparison", "Benchmark Return", benchmarkReturn)
-        strategyReturn = (self.Portfolio.TotalPortfolioValue / self.initialPortfolioValue) - 1
-        self.Plot("Cumulative Return Comparison", "Strategy Return", strategyReturn)
+        current_benchmark_price = self.benchmark.evaluate(self.time)
+        if self.initial_benchmark_price is None:
+            self.initial_benchmark_price = current_benchmark_price
+        benchmark_return = (current_benchmark_price / self.initial_benchmark_price) - 1
+        self.plot("Cumulative Return Comparison", "Benchmark Return", benchmark_return)
+        strategy_return = (self.portfolio.total_portfolio_value / self.initial_portfolio_value) - 1
+        self.plot("Cumulative Return Comparison", "Strategy Return", strategy_return)
 
-        # The current value of self.rsi is represented by self.rsi.Current.Value
-        # self.Plot("RSI Plot", "rsi", self.rsi.Current.Value)
-        # Plot all attributes of self.rsi
-        # self.Plot("RSI Plot", "rsi-averageloss", self.rsi.AverageLoss.Current.Value)
-        # self.Plot("RSI Plot", "rsi-averagegain", self.rsi.AverageGain.Current.Value)
+        # The current value of self.rsi_ is represented by self.rsi_.Current.Value
+        # self.Plot("RSI Plot", "rsi", self.rsi_.Current.Value)
+        # Plot all attributes of self.rsi_
+        # self.Plot("RSI Plot", "rsi-averageloss", self.rsi_.AverageLoss.Current.Value)
+        # self.Plot("RSI Plot", "rsi-averagegain", self.rsi_.AverageGain.Current.Value)
 
-        if self.Portfolio.Invested == False:
-            if price < self.bb.LowerBand.Current.Value and self.rsi.Current.Value < 30:
-                self.SetHoldings(self.symbol, 1)
-                self.Plot("Trade Plot", "Buy", price)
-            elif price > self.bb.UpperBand.Current.Value and self.rsi.Current.Value > 70:
-                self.SetHoldings(self.symbol, -1)
-                self.Plot("Trade Plot", "Sell", price)
+        if self.portfolio.invested == False:
+            if price < self.bb_.LowerBand.Current.Value and self.rsi_.Current.Value < 30:
+                self.set_holdings(self.symbol, 1)
+                self.plot("Trade Plot", "Buy", price)
+            elif price > self.bb_.UpperBand.Current.Value and self.rsi_.Current.Value > 70:
+                self.set_holdings(self.symbol, -1)
+                self.plot("Trade Plot", "Sell", price)
         else:
-            if self.Portfolio[self.symbol].IsLong:
-                # if price > self.bb.MiddleBand.Current.Value and self.rsi.Current.Value > 50:
-                if price > self.bb.UpperBand.Current.Value:
-                    self.Liquidate()
-                    self.Plot("Trade Plot", "Liquidate", price)
-            # elif price < self.bb.MiddleBand.Current.Value and self.rsi.Current.Value < 50:
-            elif price < self.bb.LowerBand.Current.Value:
-                self.Liquidate()
-                self.Plot("Trade Plot", "Liquidate", price)
+            if self.portfolio[self.symbol].is_long:
+                # if price > self.bb_.MiddleBand.Current.Value and self.rsi_.Current.Value > 50:
+                if price > self.bb_.UpperBand.Current.Value:
+                    self.liquidate()
+                    self.plot("Trade Plot", "Liquidate", price)
+            # elif price < self.bb_.MiddleBand.Current.Value and self.rsi_.Current.Value < 50:
+            elif price < self.bb_.LowerBand.Current.Value:
+                self.liquidate()
+                self.plot("Trade Plot", "Liquidate", price)
 
-    def OnOrderEvent(self, order_event: OrderEvent) -> None:
-        order = self.Transactions.GetOrderById(order_event.OrderId)
-        if order_event.Status == OrderStatus.Filled:
-            # self.Debug(f"Order filled: {orderEvent.Symbol}")
-            self.Debug(f"order.Type: {order.Type} OrderEvent: {order_event}")
-            self.df_order_plot.loc[len(self.df_order_plot)] = [self.Time,
-                                                               order_event.OrderId,
-                                                               order_event.Symbol,
-                                                               order_event.Status,
-                                                               order_event.Quantity,
-                                                               order_event.FillQuantity,
-                                                               order_event.FillPrice,
-                                                               order_event.OrderFee
+    def on_order_event(self, order_event: OrderEvent) -> None:
+        order = self.transactions.get_order_by_id(order_event.order_id)
+        if order_event.status == OrderStatus.FILLED:
+            # self.debug(f"Order filled: {orderEvent.Symbol}")
+            self.debug(f"order.Type: {order.type} OrderEvent: {order_event}")
+            self.df_order_plot.loc[len(self.df_order_plot)] = [self.time,
+                                                               order_event.order_id,
+                                                               order_event.symbol,
+                                                               order_event.status,
+                                                               order_event.quantity,
+                                                               order_event.fill_quantity,
+                                                               order_event.fill_price,
+                                                               order_event.order_fee
                                                                ]
 
-    def OnEndOfAlgorithm(self) -> None:
+    def on_end_of_algorithm(self) -> None:
         self.df_order_plot.to_csv(f'{LEAN_RESULTS_DIR}/df_order_plot.csv', index=False)
         self.df_timeseries.to_csv(f'{LEAN_RESULTS_DIR}/df_timeseries.csv', index=False)
-        #self.Log(self.df_timeseries)
-        self.Debug("Algorithm done")
+        #self.log(self.df_timeseries)
+        self.debug("Algorithm done")
+
+    def get_eod_stats(self):
+        # log the total equity, realized and unrealized profit
+        self.debug(f"Time: {self.time}, "
+                    f"total_profit: {self.portfolio.total_profit}, "
+                    f"total_net_profit: {self.portfolio.total_net_profit}, "
+                    f"Total total_portfolio_value: {self.portfolio.total_portfolio_value}, "
+                    f"Unrealized Profit: {self.portfolio.total_unrealised_profit}, "
+                    f"TotalFees: {self.portfolio.total_fees}, "
+                    f"Cash: {self.portfolio.cash}"
+                )
